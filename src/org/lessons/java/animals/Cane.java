1ff1389c8a82817f173cee400f01c7a9e8c10123
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-05-17 10:23:59 +0200

    exercise done

--- src/org/lessons/java/animals/Cane.java
+++ src/org/lessons/java/animals/Cane.java
@@ -1,11 +1,15 @@
 package org.lessons.java.animals;
 
-public class Cane extends Animale {
+public class Cane extends Animale implements INuotante {
 	public void verso() {
 		System.out.println("Il cane fa Bau");
 	}
 	
 	public void mangia() {
 		System.out.println("Il cane mangia carne");
+	}
+	
+	public void nuota() {
+		System.out.println("Sto nuotando!!!");
 	}
 }
--- src/org/lessons/java/animals/Delfino.java
+++ src/org/lessons/java/animals/Delfino.java
@@ -1,11 +1,15 @@
 package org.lessons.java.animals;
 
-public class Delfino extends Animale {
+public class Delfino extends Animale implements INuotante {
 	public void verso() {
 		System.out.println("Il delfino fa Boh");
 	}
 	
 	public void mangia() {
 		System.out.println("Il delfno mangia pesciolini");
+	}
+	
+	public void nuota() {
+		System.out.println("Sto nuotando!!!");
 	}
 }
